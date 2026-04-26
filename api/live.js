cf2895ba082b07d3fc0ee5eeaf3eb0dc62547511
dgurjar54975497-sys <dgurjar54975497@gmail.com>
2026-04-26 07:44:36 +0530

    Update live.js
    App

--- api/live.js
+++ api/live.js
@@ -1,1 +1,24 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://www.thesportsdb.com/api/v1/json/3/eventsday.php?s=Soccer"
+    );
 
+    const data = await response.json();
+
+    if (!data.events) {
+      return res.status(200).json({ events: [] });
+    }
+
+    const matches = data.events.map(m => ({
+      title: m.strEvent,
+      sport: "Football",
+      date: m.dateEvent
+    }));
+
+    res.status(200).json({ events: matches });
+
+  } catch (e) {
+    res.status(500).json({ events: [] });
+  }
+}
